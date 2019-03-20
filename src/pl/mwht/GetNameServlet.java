d9973c846357fb849f6d5e82bdb279b8ca4b833a
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-20 17:16:23 +0100

    add form and date getter in GetNameServlet

--- src/pl/mwht/GetNameServlet.java
+++ src/pl/mwht/GetNameServlet.java
@@ -6,14 +6,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @WebServlet(name = "GetNameServlet")
 public class GetNameServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
 
+        response.setContentType("text/html; charset=utf-8");
+
+        try {
+            out.println("<h3>" + getCurrentDate() + "</h3><br />\n" +
+                    "<form action=\"ProcessNameServlet\" method=\"POST\">\n" +
+                    "Imie: <input type=\"text\" name=\"name\" />\n" +
+                    "<input type=\"submit\" value=\"Dalej\" />\n" +
+                    "</form>");
+        } finally {
+            out.flush();
+            out.close();
+        }
+    }
+
+    private String getCurrentDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 }
