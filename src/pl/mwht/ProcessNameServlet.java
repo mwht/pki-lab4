9c47508ea117485af4910cfe62e30ee732c27ea1
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-20 17:22:52 +0100

    add processing name

--- src/pl/mwht/ProcessNameServlet.java
+++ src/pl/mwht/ProcessNameServlet.java
@@ -6,11 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ProcessNameServlet")
 public class ProcessNameServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html; charset=utf-8");
 
+        try {
+            String name = request.getParameter("name");
+            if(name != null) {
+                out.println("<h1>Czesc " + name + "</h1>");
+            }
+        } finally {
+            out.flush();
+            out.close();
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
